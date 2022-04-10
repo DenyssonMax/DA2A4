2202c8cb69bced635b85b07ea5d3854cf3e329ea
Denysson Max <maxdenyssonn@gmail.com>
2022-04-09 22:52:55 -0300

    Atualização a4

--- Atividades/A4/scripts.js
+++ Atividades/A4/scripts.js
@@ -118,7 +118,7 @@ const Utils = {
        /* value = Number(value.replace(/\,\./g, "")) * 100
         
         return value */
-        value = value = 100
+        value = value * 100
         return Math.round(value)
     },
 
