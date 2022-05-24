9c12d1558b2e8521ee48bafc3b42c07f5dc97514
Denysson Max <maxdenyssonn@gmail.com>
2022-05-24 15:28:29 -0300

    recomeçando A8

---
+++ Atividades/A8/src/components/RepositoryList.jsx
@@ -0,0 +1,14 @@
+export function RepositoryList(){
+    return (
+        <section className="respository-list">
+            <h1>Lista de Repositórios</h1>
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar o respositório</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
